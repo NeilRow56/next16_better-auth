c1edb543ad5edacba7e6967283a3b7a1ece799fd
NeilRow56 <neilrowland56@gmail.com>
2025-10-27 10:58:16 +0000

    Added drizzle and better-auth setups

---
+++ src/lib/auth-client.ts
@@ -0,0 +1,20 @@
+import { createAuthClient } from 'better-auth/react'
+
+const authClient = createAuthClient({
+  baseURL: process.env.BETTER_AUTH_URL
+})
+
+export const {
+  signIn,
+  signUp,
+  signOut,
+  useSession,
+  sendVerificationEmail,
+  revokeOtherSessions,
+  revokeSession,
+  forgetPassword,
+  resetPassword,
+  updateUser,
+  deleteUser,
+  getSession
+} = authClient
